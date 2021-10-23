a85a1f3a0632e30d5db45d66413c3bca8eac2ab4
jorika <gogoladzeg321@gmail.com>
2021-10-23 19:38:12 +0400

    commitdsa

--- src/Components/DataOfChart/DataTable.js
+++ src/Components/DataOfChart/DataTable.js
@@ -4,17 +4,34 @@ import classes from './DataTable.module.css'
 import { AiOutlineArrowDown } from "react-icons/ai"
 
 const DataTable =()=> {
-    const managedState = useSelector(state=>state)
-    // console.log(managedState)
+    const dispatch = useDispatch();
 
+    const managedState = useSelector(state=>state)
+
+
+   const object = managedState.map((value)=>
+       <ul key={value.id} className={classes.container}>
+           <li className={classes.timeStamp}>{value.timeStamp}</li>
+           <li className={classes.count}>{value.count}</li>
+           <li className={classes[value.severity]}>{value.severity} </li>
+       </ul>
+   )
+    const SortByCounterHandler=()=>{
+        dispatch({type:'sortByCount'})
+        console.log('sort is valid')
+    }
+
+    // console.log(object)
     return <Fragment>
         <div className={classes.flex}>
-            <div >TimeStamp <AiOutlineArrowDown/></div>
-            <div>Count <AiOutlineArrowDown /></div>
+            <div >TimeStamp <AiOutlineArrowDown /></div>
+            <div>Count <AiOutlineArrowDown  style={{cursor:'pointer'}}/></div>
             <div>Severity </div>
-        </div>
-        <div>{managedState}<hr/></div>
+            <button onClick={SortByCounterHandler}>click there</button>
+        </div >
+        <li>{object}</li>
     </Fragment>
 
 }
 export default DataTable;
+
--- src/Components/store/redux-demo.js
+++ src/Components/store/redux-demo.js
@@ -1,121 +1,140 @@
 import {createStore} from 'redux';
-import {Fragment} from "react";
 import './Redux.module.css';
-import classes from './Redux.module.css'
+// import classes from './Redux.module.css'
 const Data = [{
-    id: '0',
-    timeStamp:'2020-11-04 06:25:01',
+    "id": '0',
+    timeStamp:'2020-11-04 06:25:11',
     count:'44000',
-    Severity:'height',
+    severity:'height',
 
 },
     {
         id: '1',
-        timeStamp:'2020-11-03 06:25:01',
+        timeStamp:'2020-11-04 06:25:10',
         count:'24000',
-        Severity:'medium',
+        severity:'medium',
 
     },
     {
         id: '2',
-        timeStamp:'2020-11-02 06:25:01',
+        timeStamp:'2020-11-04 06:25:09',
         count:'34000',
-        Severity:'low',
+        severity:'low',
 
     },
     {
         id: '3',
-        timeStamp:'2020-11-01 06:25:01',
+        timeStamp:'2020-11-04 06:25:08',
         count:'14000',
-        Severity:'height',
+        severity:'height',
 
     },
     {
         id: '4',
-        timeStamp:'2020-10-04 06:25:01',
+        timeStamp:'2020-10-04 06:25:07',
         count:'5000',
-        Severity:'low',
+        severity:'low',
 
     },
     {
         id: '5',
-        timeStamp:'2020-10-05 06:25:01',
+        timeStamp:'2020-10-04 06:25:06',
         count:'4000',
-        Severity:'medium',
+        severity:'medium',
 
     },
     {
         id: '6',
-        timeStamp:'2020-10-04 06:25:01',
+        timeStamp:'2020-10-04 06:25:05',
         count:'14000',
-        Severity:'low',
+        severity:'low',
 
     },
     {
         id: '7',
-        timeStamp:'2020-10-04 06:25:01',
+        timeStamp:'2020-10-04 06:25:04',
         count:'2000',
-        Severity:'height',
+        severity:'height',
 
     },
     {
         id: '8',
-        timeStamp:'2020-10-04 06:25:01',
+        timeStamp:'2020-10-04 06:25:03',
         count:'3000',
-        Severity:'height',
+        severity:'height',
 
     },
     {
         id: '9',
-        timeStamp:'2020-10-04 06:25:01',
+        timeStamp:'2020-10-04 06:25:02',
         count:'4000',
-        Severity:'height',
+        severity:'height',
 
     },
 ]
 
 const ManagedReducer =(state=Data, action)=>{
-    switch (action.type) {
-        case 'SortCount':
-            return <Fragment>
-                {state.map((value)=>(
-                    <ul key={value.id} className={classes.container}>
-                        <li className={classes.timeStamp}>{value.timeStamp}</li>
-                        <li className={classes.count}>{value.count}</li>
-                        <li className={classes[value.Severity]}>{value.Severity} </li>
+    // state.sort((a,b)=>b.count-a.count)
+    // state.sort((a,b)=>a.timeStamp.localeCompare(b.timeStamp))
 
-                    </ul>
+    if (action.type=== 'sortByCount'){
+        return {
 
-                ))}
-            </Fragment>;
-        case 'sortTimeStamp':
-            return <Fragment>
-                {state.map((value)=>(
-                    <ul key={value.id} className={classes.container}>
-                        <li className={classes.timeStamp}>{value.timeStamp}</li>
-                        <li className={classes.count}>{value.count}</li>
-                        <li className={classes[value.Severity]}>{value.Severity} </li>
-
-                    </ul>
-
-                ))}
-            </Fragment>;
-
-        default:
-    return <Fragment>
-        {state.map((value)=>(
-            <ul key={value.id} className={classes.container}>
-                <li className={classes.timeStamp}>{value.timeStamp}</li>
-                <li className={classes.count}>{value.count}</li>
-                <li className={classes[value.Severity]}>{value.Severity} </li>
-
-            </ul>
-
-        ))}
-    </Fragment>
+       state: state.sort((a,b)=>a.count.localeCompare(b.count))
+        }
+    }else{
+        return state;
     }
-}
 
-const store = createStore(ManagedReducer)
+    // return state;
+
+
+
+}
+        const store = createStore(ManagedReducer)
 
 export default store;
+
+//     switch (action.type) {
+//         case 'SortCount':
+//             return <Fragment>
+//                 {state.sort((a, b) => b.count - a.count).map((value)=>(
+//                     <ul key={value.id} className={classes.container}>
+//                         <li className={classes.timeStamp}>{value.timeStamp}</li>
+//                         <li className={classes.count}>{value.count}</li>
+//                         <li className={classes[value.Severity]}>{value.Severity} </li>
+//
+//                     </ul>
+//                 ))}
+//             </Fragment>;
+//
+//
+//
+//         case 'sortTimeStamp':
+//             return <Fragment>
+//                 {state.map((value)=>(
+//                     <ul key={value.id} className={classes.container}>
+//                         <li className={classes.timeStamp}>{value.timeStamp}</li>
+//                         <li className={classes.count}>{value.count}</li>
+//                         <li className={classes[value.Severity]}>{value.Severity} </li>
+//
+//                     </ul>
+//
+//                 ))}
+//             </Fragment>;
+//
+//         default:
+//             return <Fragment>
+//                 {state.map((value)=>(
+//                     <ul key={value.id} className={classes.container}>
+//                         <li className={classes.timeStamp}>{value.timeStamp}</li>
+//                         <li className={classes.count}>{value.count}</li>
+//                         <li className={classes[value.Severity]}>{value.Severity} </li>
+//
+//                     </ul>
+//
+//                 ))}
+//             </Fragment>
+//     }
+// }
+//
